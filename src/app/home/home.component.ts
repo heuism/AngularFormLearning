9a0e61716c6b7207ef77bd292e178856440e5227
Hien Curve <hien@curvetomorrow.com.au>
2023-10-30 13:43:19 +1100

    [main] added occupation dropdown to form

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,46 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
   employees: Employee[];
   companyForm: FormGroup;
+
+  private fb = inject(FormBuilder);
   constructor() {
     this.employees = [];
     this.companyForm = new FormGroup({
       companyName: new FormControl('', { nonNullable: true }),
       companyNumber: new FormControl('', { nonNullable: true }),
       companyAddress: new FormControl('', { nonNullable: true }),
+      employees: this.fb.array([]),
     });
   }
+
+  get employeeList() {
+    return this.companyForm.controls['employees'] as FormArray;
+  }
+
   addNewEmployee() {
-    this.employees.push(new Employee());
-    console.log(this.employees);
+    const employees = this.employeeList;
+    employees.push(
+      this.fb.group({
+        name: '',
+        fromDate: '',
+        toDate: '',
+        salary: '',
+        occupation: '',
+      })
+    );
+  }
+
+  onChangeDate(event: any, index: number) {
+    console.log(event.target.value);
+    // this.employeeList.value[index].fromDate = event.target.value;
   }
 
   saveForm() {
@@ -27,6 +48,10 @@ export class HomeComponent {
   }
 }
 
-class Employee {
-
+export interface Employee {
+  name: string;
+  fromDate: string;
+  toDate: string;
+  salary: string;
+  occupation: string;
 }
